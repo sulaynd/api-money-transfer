f4dfb1a6365f32d629446edd828f3f341032752f
souleye NDIAYE <sulaynd@gmail.com>
2024-09-08 15:35:52 -0400

    first commit

---
+++ ratings-service/src/test/java/com/loulysoft/moneytransfer/ratings/RatingsServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.loulysoft.moneytransfer.ratings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(ContainersConfig.class)
+@SpringBootTest
+class RatingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
