d025c14c5a1b123a539a3f3bc89084ead6901eff
souleye NDIAYE <sulaynd@gmail.com>
2024-09-09 10:51:03 -0400

    Ratings service module setup

--- ratings-service/src/main/java/com/loulysoft/moneytransfer/ratings/RatingsServiceApplication.java
+++ ratings-service/src/main/java/com/loulysoft/moneytransfer/ratings/RatingsServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RatingsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RatingsServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RatingsServiceApplication.class, args);
+    }
 }
--- ratings-service/src/test/java/com/loulysoft/moneytransfer/ratings/TestRatingsServiceApplication.java
+++ ratings-service/src/test/java/com/loulysoft/moneytransfer/ratings/TestRatingsServiceApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 
 public class TestRatingsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(RatingsServiceApplication::main).with(ContainersConfig.class).run(args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.from(RatingsServiceApplication::main)
+                .with(ContainersConfig.class)
+                .run(args);
+    }
 }
